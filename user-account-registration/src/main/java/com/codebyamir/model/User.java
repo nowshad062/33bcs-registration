2d41979166aee38db816ce6a2f0dae6512df7598
LAPTOP-AC9DN5HR\nowsh <nowshad062@gmail.com>
2020-08-14 21:54:54 -0700

    Initial Commit

--- user-account-registration/src/main/java/com/codebyamir/model/User.java
+++ user-account-registration/src/main/java/com/codebyamir/model/User.java
@@ -12,38 +12,54 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.Transient;
 
 @Entity
-@Table(name = "user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id")
 	private int id;
-	
-	@Column(name = "email", nullable = false, unique = true)
+
+	@Column(nullable = false, unique = true)
 	@Email(message = "Please provide a valid e-mail")
 	@NotEmpty(message = "Please provide an e-mail")
 	private String email;
-	
-	@Column(name = "password")
+
 	@Transient
 	private String password;
-	
-	@Column(name = "first_name")
-	@NotEmpty(message = "Please provide your first name")
-	private String firstName;
-	
-	@Column(name = "last_name")
-	@NotEmpty(message = "Please provide your last name")
-	private String lastName;
-	
-	@Column(name = "enabled")
+
+	@NotEmpty(message = "Please provide your name")
+	private String name;
+
+	@NotEmpty(message = "Please provide your name in Bengali")
+	private String nameBangla;
+
+	@NotEmpty(message = "Please Enter your occupation")
+	private String occupation;
+
+	@NotEmpty(message = "Please Enter your Pay Scale")
+	private String payscale;
+
+	@NotEmpty(message = "Please Enter Current Work Place")
+	private String currentWorkplace;
+
+	@NotEmpty(message = "Please Enter Current Work Place")
+	private String fatherName;
+
+	@NotEmpty(message = "Please Enter Current Work Place")
+	private String motherName;
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+
+
+	@NotEmpty(message = "Please Enter Current Work Place")
+	private String presentAddress;
+
 	private boolean enabled;
-	
-	@Column(name = "confirmation_token")
+
 	private String confirmationToken;
 
-	
 	public String getConfirmationToken() {
 		return confirmationToken;
 	}
@@ -51,7 +67,6 @@ public class User {
 	public void setConfirmationToken(String confirmationToken) {
 		this.confirmationToken = confirmationToken;
 	}
-
 
 	public int getId() {
 		return id;
@@ -69,20 +84,12 @@ public class User {
 		this.password = password;
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public String getName() {
+		return name;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
@@ -101,5 +108,57 @@ public class User {
 		this.enabled = value;
 	}
 
+
+	public String getNameBangla() {
+		return nameBangla;
+	}
+
+	public void setNameBangla(String nameBangla) {
+		this.nameBangla = nameBangla;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getPayscale() {
+		return payscale;
+	}
+
+	public void setPayscale(String payscale) {
+		this.payscale = payscale;
+	}
+
+	public String getCurrentWorkplace() {
+		return currentWorkplace;
+	}
+
+	public void setCurrentWorkplace(String currentWorkplace) {
+		this.currentWorkplace = currentWorkplace;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public String getPresentAddress() {
+		return presentAddress;
+	}
+
+	public void setPresentAddress(String presentAddress) {
+		this.presentAddress = presentAddress;
+	}
 
 }
