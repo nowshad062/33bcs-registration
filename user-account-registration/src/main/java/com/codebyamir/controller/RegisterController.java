29289a25dfbda5a8be0169168204186c4f2a4e98
LAPTOP-AC9DN5HR\nowsh <nowshad062@gmail.com>
2020-08-16 00:42:23 -0700

    Updated Registration form

--- user-account-registration/src/main/java/com/codebyamir/controller/RegisterController.java
+++ user-account-registration/src/main/java/com/codebyamir/controller/RegisterController.java
@@ -1,5 +1,10 @@
 package com.codebyamir.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 import java.util.UUID;
 
@@ -10,10 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -29,6 +37,8 @@ public class RegisterController {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	private UserService userService;
 	private EmailService emailService;
+
+	private final String UPLOAD_DIR = "uploads/";
 	
 	@Autowired
 	public RegisterController(BCryptPasswordEncoder bCryptPasswordEncoder,
@@ -44,6 +54,32 @@ public class RegisterController {
 		modelAndView.addObject("user", user);
 		modelAndView.setViewName("register");
 		return modelAndView;
+	}
+
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
+
+		// check if file is empty
+		if (file.isEmpty()) {
+			attributes.addFlashAttribute("message", "Please select a file to upload.");
+			return "redirect:/";
+		}
+
+		// normalize the file path
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+		// save the file on the local file system
+		try {
+			Path path = Paths.get(UPLOAD_DIR + fileName);
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// return success response
+		attributes.addFlashAttribute("message", "You successfully uploaded " + fileName + '!');
+
+		return "redirect:/";
 	}
 	
 	// Process form input data
--- user-account-registration/src/main/java/com/codebyamir/model/User.java
+++ user-account-registration/src/main/java/com/codebyamir/model/User.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -22,6 +21,8 @@ public class User {
 	@Email(message = "Please provide a valid e-mail")
 	@NotEmpty(message = "Please provide an e-mail")
 	private String email;
+
+	private String phone;
 
 	@Transient
 	private String password;
@@ -47,14 +48,119 @@ public class User {
 	@NotEmpty(message = "Please Enter Current Work Place")
 	private String motherName;
 
-	public String getFatherName() {
-		return fatherName;
-	}
-
-
-
 	@NotEmpty(message = "Please Enter Current Work Place")
 	private String presentAddress;
+
+
+	@NotEmpty(message = "Please Enter Permanent Address")
+	private String permanentAddress;
+
+	@NotEmpty(message = "Please Enter Religion")
+	private String religion;
+
+	@NotEmpty(message = "Please Enter Sex")
+	private String sex;
+
+	@NotEmpty(message = "Please Enter Marrital Status")
+	private String mStatus;
+
+	@NotEmpty(message = "Please Enter National ID No")
+	private String NID;
+
+	private String TIN;
+
+
+
+	@NotEmpty(message = "Please Enter Date of Birth")
+	private String dob;
+
+	@NotEmpty(message = "Please Enter PRL Date")
+	private String prlDate;
+
+	@NotEmpty(message = "Please Enter Posting Status")
+	private String postingStatus;
+
+	@NotEmpty(message = "Please Enter Posted As")
+	private String postedAs;
+
+	@NotEmpty(message = "Please Enter Post ID")
+	private String postId;
+
+	private String HRISId;
+
+	private String codeNo;
+
+	private String bcsBatchNo;
+
+	private String noStudyDept;
+
+	private String originalDesignation;
+
+	private String acrAvailability;
+
+	private String profDiscipline;
+
+	private String profCategory;
+
+	private String jobStatus;
+
+	private String tribe;
+
+	private String isFreedomFighter;
+
+	private String isLiveGovtQuarter;
+
+	private String jdHealthService;
+
+	private String jdCurrentPlace;
+
+	private String jdCurrentDesignation;
+
+	private String currentBasicPay;
+
+	private String lastDesignation;
+
+	private String lastGoNo;
+
+	private String lastsSerialNo;
+
+	private String lastPromoDate;
+
+	private String bcsGo;
+
+	private String bcsSerialNo;
+
+	private String bcsDate;
+
+	private String istSerialNo;
+
+	private String istGo;
+
+	private String istDate;
+
+	private String serviceGo;
+
+	private String serviceGoDate;
+
+	private String serviceSerialNo;
+
+	private String serviceDate;
+
+	private String appointmentGo;
+
+	private String appointmentSerialNo;
+
+	private String appointmentDate;
+
+	private String isSeniorScalePass;
+
+	private String isVillageExp;
+
+	private String mbbsYear;
+
+	private String hscYear;
+
+	private String sscYear;
 
 	private boolean enabled;
 
@@ -91,6 +197,13 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
 
 	public String getEmail() {
 		return email;
@@ -161,4 +274,403 @@ public class User {
 		this.presentAddress = presentAddress;
 	}
 
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+
+	public void setPermanentAddress(String permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+
+	public String getReligion() {
+		return religion;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getmStatus() {
+		return mStatus;
+	}
+
+	public void setmStatus(String mStatus) {
+		this.mStatus = mStatus;
+	}
+
+	public String getNID() {
+		return NID;
+	}
+
+	public void setNID(String NID) {
+		this.NID = NID;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getPrlDate() {
+		return prlDate;
+	}
+
+	public void setPrlDate(String prlDate) {
+		this.prlDate = prlDate;
+	}
+
+	public String getPostingStatus() {
+		return postingStatus;
+	}
+
+	public void setPostingStatus(String status) {
+		this.postingStatus = status;
+	}
+
+	public String getPostedAs() {
+		return postedAs;
+	}
+
+	public void setPostedAs(String postedAs) {
+		this.postedAs = postedAs;
+	}
+
+	public String getPostId() {
+		return postId;
+	}
+
+	public void setPostId(String postId) {
+		this.postId = postId;
+	}
+
+	public String getCodeNo() {
+		return codeNo;
+	}
+
+	public void setCodeNo(String codeNo) {
+		this.codeNo = codeNo;
+	}
+
+	public String getBcsBatchNo() {
+		return bcsBatchNo;
+	}
+
+	public void setBcsBatchNo(String bcsBatchNo) {
+		this.bcsBatchNo = bcsBatchNo;
+	}
+
+	public String getNoStudyDept() {
+		return noStudyDept;
+	}
+
+	public void setNoStudyDept(String noStudyDept) {
+		this.noStudyDept = noStudyDept;
+	}
+
+	public String getOriginalDesignation() {
+		return originalDesignation;
+	}
+
+	public void setOriginalDesignation(String originalDesignation) {
+		this.originalDesignation = originalDesignation;
+	}
+
+	public String getAcrAvailability() {
+		return acrAvailability;
+	}
+
+	public void setAcrAvailability(String acrAvailability) {
+		this.acrAvailability = acrAvailability;
+	}
+
+	public String getProfDiscipline() {
+		return profDiscipline;
+	}
+
+	public void setProfDiscipline(String profDiscipline) {
+		this.profDiscipline = profDiscipline;
+	}
+
+	public String getProfCategory() {
+		return profCategory;
+	}
+
+	public void setProfCategory(String profCategory) {
+		this.profCategory = profCategory;
+	}
+
+	public String getJobStatus() {
+		return jobStatus;
+	}
+
+	public void setJobStatus(String jobStatus) {
+		this.jobStatus = jobStatus;
+	}
+
+	public String getTribe() {
+		return tribe;
+	}
+
+	public void setTribe(String tribe) {
+		this.tribe = tribe;
+	}
+
+	public String getIsFreedomFighter() {
+		return isFreedomFighter;
+	}
+
+	public void setIsFreedomFighter(String isFreedomFighter) {
+		this.isFreedomFighter = isFreedomFighter;
+	}
+
+	public String getIsLiveGovtQuarter() {
+		return isLiveGovtQuarter;
+	}
+
+	public void setIsLiveGovtQuarter(String isLiveGovtQuarter) {
+		this.isLiveGovtQuarter = isLiveGovtQuarter;
+	}
+
+	public String getJdHealthService() {
+		return jdHealthService;
+	}
+
+	public void setJdHealthService(String jdHealthService) {
+		this.jdHealthService = jdHealthService;
+	}
+
+	public String getJdCurrentDesignation() {
+		return jdCurrentDesignation;
+	}
+
+	public void setJdCurrentDesignation(String jdCurrentDesignation) {
+		this.jdCurrentDesignation = jdCurrentDesignation;
+	}
+
+	public String getCurrentBasicPay() {
+		return currentBasicPay;
+	}
+
+	public void setCurrentBasicPay(String currentBasicPay) {
+		this.currentBasicPay = currentBasicPay;
+	}
+
+	public String getJdCurrentPlace() {
+		return jdCurrentPlace;
+	}
+
+	public void setJdCurrentPlace(String jdCurrentPlace) {
+		this.jdCurrentPlace = jdCurrentPlace;
+	}
+
+	public String getLastDesignation() {
+		return lastDesignation;
+	}
+
+	public void setLastDesignation(String lastDesignation) {
+		this.lastDesignation = lastDesignation;
+	}
+
+	public String getLastGoNo() {
+		return lastGoNo;
+	}
+
+	public void setLastGoNo(String lastGoNo) {
+		this.lastGoNo = lastGoNo;
+	}
+
+	public String getLastsSerialNo() {
+		return lastsSerialNo;
+	}
+
+	public void setLastsSerialNo(String lastsSerialNo) {
+		this.lastsSerialNo = lastsSerialNo;
+	}
+
+	public String getLastPromoDate() {
+		return lastPromoDate;
+	}
+
+	public void setLastPromoDate(String lastPromoDate) {
+		this.lastPromoDate = lastPromoDate;
+	}
+
+	public String getBcsGo() {
+		return bcsGo;
+	}
+
+	public void setBcsGo(String bcsGo) {
+		this.bcsGo = bcsGo;
+	}
+
+	public String getBcsSerialNo() {
+		return bcsSerialNo;
+	}
+
+	public void setBcsSerialNo(String bcsSerialNo) {
+		this.bcsSerialNo = bcsSerialNo;
+	}
+
+	public String getBcsDate() {
+		return bcsDate;
+	}
+
+	public void setBcsDate(String bcsDate) {
+		this.bcsDate = bcsDate;
+	}
+
+	public String getIstSerialNo() {
+		return istSerialNo;
+	}
+
+	public void setIstSerialNo(String istSerialNo) {
+		this.istSerialNo = istSerialNo;
+	}
+
+	public String getIstGo() {
+		return istGo;
+	}
+
+	public void setIstGo(String istGo) {
+		this.istGo = istGo;
+	}
+
+	public String getIstDate() {
+		return istDate;
+	}
+
+	public void setIstDate(String istDate) {
+		this.istDate = istDate;
+	}
+
+	public String getServiceGo() {
+		return serviceGo;
+	}
+
+	public void setServiceGo(String serviceGo) {
+		this.serviceGo = serviceGo;
+	}
+
+	public String getServiceGoDate() {
+		return serviceGoDate;
+	}
+
+	public void setServiceGoDate(String serviceGoDate) {
+		this.serviceGoDate = serviceGoDate;
+	}
+
+	public String getServiceSerialNo() {
+		return serviceSerialNo;
+	}
+
+	public void setServiceSerialNo(String serviceSerialNo) {
+		this.serviceSerialNo = serviceSerialNo;
+	}
+
+	public String getServiceDate() {
+		return serviceDate;
+	}
+
+	public void setServiceDate(String serviceDate) {
+		this.serviceDate = serviceDate;
+	}
+
+	public String getAppointmentGo() {
+		return appointmentGo;
+	}
+
+	public void setAppointmentGo(String appointmentGo) {
+		this.appointmentGo = appointmentGo;
+	}
+
+	public String getAppointmentSerialNo() {
+		return appointmentSerialNo;
+	}
+
+	public void setAppointmentSerialNo(String appointmentSerialNo) {
+		this.appointmentSerialNo = appointmentSerialNo;
+	}
+
+	public String getAppointmentDate() {
+		return appointmentDate;
+	}
+
+	public void setAppointmentDate(String appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+	public String getIsSeniorScalePass() {
+		return isSeniorScalePass;
+	}
+
+	public void setIsSeniorScalePass(String isSeniorScalePass) {
+		this.isSeniorScalePass = isSeniorScalePass;
+	}
+
+	public String getIsVillageExp() {
+		return isVillageExp;
+	}
+
+	public void setIsVillageExp(String isVillageExp) {
+		this.isVillageExp = isVillageExp;
+	}
+
+	public String getMbbsYear() {
+		return mbbsYear;
+	}
+
+	public void setMbbsYear(String mbbsYear) {
+		this.mbbsYear = mbbsYear;
+	}
+
+	public String getHscYear() {
+		return hscYear;
+	}
+
+	public void setHscYear(String hscYear) {
+		this.hscYear = hscYear;
+	}
+
+	public String getSscYear() {
+		return sscYear;
+	}
+
+	public void setSscYear(String sscYear) {
+		this.sscYear = sscYear;
+	}
+
+	public String getTIN() {
+		return TIN;
+	}
+
+	public void setTIN(String TIN) {
+		this.TIN = TIN;
+	}
+
+	public String getHRISId() {
+		return HRISId;
+	}
+
+	public void setHRISId(String HRISId) {
+		this.HRISId = HRISId;
+	}
 }
